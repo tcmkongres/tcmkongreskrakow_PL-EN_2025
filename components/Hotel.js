8a588c0d66c1c295e3ba76bdf4110b601cc497ee
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-04-27 20:55:10 +0200

    Dodaje nowy hotel

--- components/Hotel.js
+++ components/Hotel.js
@@ -1,72 +1,48 @@
-import hotel1 from "@/public/images/hotel1.jpg";
-import hotel3 from "@/public/images/hotel3.jpg";
-import hotel2 from "@/public/images/hotel2.jpg";
-import hotel4 from "@/public/images/hotel4.jpg";
+import hotel1 from "@/public/images/hotel1.png";
 import Image from "next/image";
 import { useTranslation } from "next-i18next";
 
 export default function Hotel() {
   const { t } = useTranslation("common");
   return (
-    <div className="overflow-hidden bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
-          <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              {t("tytul_Hotel")}
-            </h2>
-            <p className="mt-6 text-xl leading-8 text-gray-600">
-              {t("adres_Hotel")}
-            </p>
-            <p className="mt-6 text-base leading-7 text-gray-600">
-              {t("opis1_Hotel")}
-            </p>
-            <p className="mt-6 text-base leading-7 text-gray-600">
-              {t("opis2_Hotel")}
-            </p>
-            <div className="mt-10 flex">
-              <a
-                href="http://galaxyhotel.pl/en/h24_code/TCMKongres/p2019-10-10/w2019-10-13/"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                {t("rezerwuj_Hotel")} <span aria-hidden="true">&rarr;</span>
-              </a>
+      <div className="overflow-hidden bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="grid grid-cols-1 gap-16 lg:grid-cols-2 lg:items-center">
+            <div>
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                {t("tytul_Hotel")}
+              </h2>
+              <p className="mt-6 text-xl leading-8 text-gray-600">
+                {t("adres_Hotel")}
+              </p>
+              <p className="mt-6 text-base leading-7 text-gray-600">
+                {t("opis1_Hotel")}
+              </p>
+              <div className="mt-10 flex flex-wrap gap-4">
+                <a
+                    href="https://www.booking.com/Share-V2eIwP"
+                    className="rounded-md bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  {t("rezerwuj_Hotel")} <span aria-hidden="true">&rarr;</span>
+                </a>
+                <a
+                    href="https://www.qubushotel.com/pl/hotel-krakow/"
+                    className="rounded-md bg-gray-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+                >
+                  {t("link_Hotel")} <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
-            <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+
+            <div className="flex justify-center lg:justify-end">
               <Image
-                src={hotel1}
-                alt=""
-                className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                  src={hotel1}
+                  alt=""
+                  className="w-full max-w-md rounded-2xl bg-gray-50 object-cover"
               />
-            </div>
-            <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
-              <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
-                <Image
-                  src={hotel2}
-                  alt=""
-                  className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
-                />
-              </div>
-              <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
-                <Image
-                  src={hotel4}
-                  alt=""
-                  className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
-                />
-              </div>
-              <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
-                <Image
-                  src={hotel3}
-                  alt=""
-                  className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
-                />
-              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
