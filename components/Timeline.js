a327be7b756d0373cfd0e37773c7330f37942c13
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-04-27 21:11:50 +0200

    poprzednie edycje - dodaje poprzednia edycja z 2023

--- components/Timeline.js
+++ components/Timeline.js
@@ -10,7 +10,7 @@ export default function Timeline() {
         </h2>
       </div>
       <div className="mx-auto max-w-7xl mt-16 px-6 lg:px-8 sm:mt-20">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-4">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <div>
             <time
               dateTime="14-16.10.2016"
@@ -117,6 +117,33 @@ export default function Timeline() {
             <p className="mt-1 text-base leading-7 text-gray-600">
               {t("tcm4_opis")}
             </p>
+          </div>
+          <div>
+            <time
+                dateTime="13-15.10.2023"
+                className="flex items-center text-sm font-semibold leading-6 text-indigo-600"
+            >
+              <svg
+                  viewBox="0 0 4 4"
+                  className="mr-4 h-1 w-1 flex-none"
+                  aria-hidden="true"
+              >
+                <circle cx="2" cy="2" r="2" fill="currentColor"></circle>
+              </svg>
+              10.2018
+              <div
+                  className="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0"
+                  aria-hidden="true"
+              ></div>
+            </time>
+            <p className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">
+              {t("tcm1_tytul")}
+              {" 2023"}
+            </p>
+            <p className="mt-1 text-base leading-7 text-gray-600">
+              {t("tcm5_opis")}
+            </p>
+
           </div>
         </div>
       </div>
