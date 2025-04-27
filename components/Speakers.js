d37a0ba9144830f5169f0f4d4df9426bf7a03326
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-04-27 20:35:07 +0200

    poprawka wielojezycznosci dla wykladowcow

--- components/Speakers.js
+++ components/Speakers.js
@@ -8,6 +8,7 @@ import Mietka from "@/public/images/Mietka.png";
 import Wojniusz from "@/public/images/Wojniusz.jpg";
 import Fraussen from "@/public/images/Fraussen.png";
 import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
 
 const people = [
   {
@@ -53,41 +54,49 @@ const people = [
 ];
 
 people.sort((a, b) =>
-  a.name.localeCompare(b.name, "pl", { sensitivity: "base" }),
+    a.name.localeCompare(b.name, "pl", { sensitivity: "base" }),
 );
+
 export default function Speakers() {
   const { t } = useTranslation("common");
+  const router = useRouter();
+  const { locale } = router;
+
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl sm:text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            {t("tytul_Speakers1")}
-          </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            {t("tytul_Speakers2")}
-          </p>
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              {t("tytul_Speakers1")}
+            </h2>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              {t("tytul_Speakers2")}
+            </p>
+          </div>
+          <ul
+              role="list"
+              className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
+          >
+            {people.map((person) => {
+              // Jeśli język to "en", dodaj "/en" do ścieżki
+              const localizedUrl = locale === "en" ? `/en${person.url}` : person.url;
+              return (
+                  <a key={person.name} href={localizedUrl}>
+                    <li>
+                      <Image
+                          className="aspect-[4/5] w-full rounded-2xl object-cover"
+                          src={person.imageUrl}
+                          alt={person.name}
+                      />
+                      <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-center text-gray-900">
+                        {person.name}
+                      </h3>
+                    </li>
+                  </a>
+              );
+            })}
+          </ul>
         </div>
-        <ul
-          role="list"
-          className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
-        >
-          {people.map((person) => (
-            <a key={person.name} href={person.url}>
-              <li>
-                <Image
-                  className="aspect-[4/5] w-full rounded-2xl object-cover"
-                  src={person.imageUrl}
-                  alt=""
-                />
-                <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-center text-gray-900">
-                  {person.name}
-                </h3>
-              </li>
-            </a>
-          ))}
-        </ul>
       </div>
-    </div>
   );
 }
