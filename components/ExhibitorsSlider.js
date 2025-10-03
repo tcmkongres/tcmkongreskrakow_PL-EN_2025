57ffdd1a03bca6bf12ee5b481ca1954e20cc013b
darek <d.kwiek@tcmsklep.pl>
2025-10-03 17:45:07 +0200

    Usunięto 2 logo ze slidra wystawców oraz dodanie jednego

--- components/ExhibitorsSlider.js
+++ components/ExhibitorsSlider.js
@@ -1,16 +1,17 @@
 import Image from "next/image";
 
 import logo1 from "@/public/images/exhibitors/acuart_yb_logo_02.jpg";
-import logo2 from "@/public/images/exhibitors/logo-wystawcy.png";
-import logo3 from "@/public/images/exhibitors/logo cmc.jpg";
-import logo4 from "@/public/images/exhibitors/logoTOMO.jpg";
-import logo5 from "@/public/images/exhibitors/logo OP szkola naturalnego NEW-09.jpg";
-import logo6 from "@/public/images/exhibitors/Delewin logotyp.png";
-import logo7 from "@/public/images/exhibitors/TCM BOHEMIA_PNG.png";
-import logo8 from "@/public/images/exhibitors/TCMsklep-logo-czerwone-800x133px.png";
-import logo9 from "@/public/images/exhibitors/Logo_DocSave2021_10cm_600dpi.jpg";
+// import logo2 from "@/public/images/exhibitors/logo-wystawcy.png";
+import logo2 from "@/public/images/exhibitors/logo cmc.jpg";
+import logo3 from "@/public/images/exhibitors/logoTOMO.jpg";
+import logo4 from "@/public/images/exhibitors/logo OP szkola naturalnego NEW-09.jpg";
+// import logo6 from "@/public/images/exhibitors/Delewin logotyp.png";
+import logo5 from "@/public/images/exhibitors/TCM BOHEMIA_PNG.png";
+import logo6 from "@/public/images/exhibitors/TCMsklep-logo-czerwone-800x133px.png";
+import logo7 from "@/public/images/exhibitors/Logo_DocSave2021_10cm_600dpi.jpg";
+import logo8 from "@/public/images/exhibitors/Herbprime_Logo_Final-02.png";
 
-const logos = [logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8, logo9];
+const logos = [logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8];
 
 export default function ExhibitorsSlider() {
   return (
